81e06374139491e560369f6b7bb73db429bf653a
Manjul Tamrakar <manjultamrakar4@gmail.com>
2025-04-24 22:15:41 +0545

    changed navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -61,8 +61,8 @@ const Navbar: React.FC = () => {
               <span className="sr-only">Toggle menu</span>
             </Button>
           </SheetTrigger>
-          <SheetContent side="right" className="w-[240px] sm:w-[300px]">
-            <div className="flex flex-col space-y-4 mt-8">
+          <SheetContent side="right" className="w-full">
+            <div className="flex flex-col space-y-4 h-full items-center justify-center">
               {links.map(link => (
                 <a
                   key={link.href}
